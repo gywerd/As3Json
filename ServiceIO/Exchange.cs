b29b01d7c99f991e1d239f46d3ffa935eee6cd12
gywerd <gywerd@gmail.com>
2018-10-26 10:27:12 +0200

    Basic Structure

---
+++ ServiceIO/Exchange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceIO
+{
+    public class Exchange
+    {
+        public string GetCurrencyFromOpenExchangeRate()
+        {
+            string result = "";
+
+            return result;
+
+        }
+
+    }
+}
